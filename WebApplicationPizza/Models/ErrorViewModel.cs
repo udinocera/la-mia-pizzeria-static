19c0ec25be148bd11c9452bdbbdcc49677313d0e
udinocera <udinocera@gmail.com>
2022-05-09 01:11:24 +0200

    Aggiungere i file di progetto.

---
+++ WebApplicationPizza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationPizza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
